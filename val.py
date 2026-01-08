576ccbd5c7cdfa636d60f58dd7f59b768d894388
Meow <2775921733@qq.com>
2026-01-08 15:09:29 +0800

    Update val.py

--- val.py
+++ val.py
@@ -29,7 +29,7 @@ def main(args):
     steps = os.listdir(args.save_dir)
     results = []
 
-    for s in range(len(steps)):
+    for s in range(1, len(steps)):
         print(f'\n{"=" * 20}\n开始评估数据集 Step: {s}\n{"=" * 20}')
 
         # 构造路径
