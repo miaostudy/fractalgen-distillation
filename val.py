a573dc6249a5ccfd8ae8a2875e07884872eeebb7
Meow <2775921733@qq.com>
2026-01-08 14:42:37 +0800

    Update val.py

--- val.py
+++ val.py
@@ -3,8 +3,14 @@ import argparse
 
 
 def main(args):
-    subprocess.run(['scripts/run_val.sh', args.save_dir], capture_output=True, text=True)  # arg1, arg2是传给脚本的参数
-
+    try:
+        results = subprocess.run(['scripts/run_val.sh', args.save_dir], capture_output=True, text=True)
+        print("STDOUT:")
+        print(results.stdout)
+        print("STDERR:")
+        print(results.stderr
+    except subprocess.CalledProcessError as e:
+        print(e)
 if __name__ ==  'main':
     parser = argparse.ArgumentParser()
     parser.add_argument('save_dir', default='/data2/wlf/fractalgen-distillation/nette', type=float)
